7e567e6010aa58767caa114eb09f43d07578b108
secretID2 <secretidentity009@gmail.com>
2018-01-17 14:10:02 +0000

    Uploading files with threads

--- EstudoPessoal/MLServer/SimpleHttpServerMultiClient.py
+++ EstudoPessoal/MLServer/SimpleHttpServerMultiClient.py
@@ -12,14 +12,59 @@ import pandas as pd
 import numpy as np
 import os
 import time
+import ML_MegaFunction as ml
+import threading
 
 i=0
 username=''
 password=''
+secret='some-secret-key'
+
+
+
+clients={}
+clients_upload_threads={}
+clients_run_threads={}
+class Client:
+    
+    p=ml.Predictor()
+    dataset=None 
+    def __init__(self,username,password):
+        self.username=username
+        self.password=password
+        
+    def Run(self):
+        self.predictor=self.p.ReturnPredictor(self.dataset)
+        
+    def Loading(self):
+        return self.p.loading
+    
+    def uploadFile(self,save_path):
+        upload = bt.request.files.get('upload')
+        name, ext = os.path.splitext(upload.filename)
+    
+        #save_path='files/Template'
+        upload.save(save_path, overwrite=True) # appends upload.filename automatically
+        print("\n\nbefore\n\n")
+        self.dataset=pd.read_csv(save_path+upload.filename)
+        print("\n\nAfter\n\n")
 
 def getUsers():
    data = np.genfromtxt('files/Users.txt',dtype=str, delimiter=',')
    return data
+
+
+def validate_user(secret):
+    global clients
+    for c in clients:
+        key = bt.request.get_cookie(clients[c].username, secret=secret)
+        if key:
+            return True
+    return False
+
+
+
+#_______Main___________________________
 
 Users=getUsers()
 print(Users)
@@ -44,26 +89,23 @@ def ShowMainPage():
     
 @bt.post('/login')
 def do_login():
+    global clients
+    global secret
+    
     username = bt.request.forms.get('username')
     password = bt.request.forms.get('password')
     if check_login(username, password):
-        bt.response.set_cookie("account", username+' '+password, secret='some-secret-key')
+        client=Client(username,password)
+        clients[username]=client
+        bt.response.set_cookie(username, password, secret=secret)
         return bt.redirect('/restricted')
     else:
         return "<p>Login failed.</p>"
 
 @bt.get('/restricted')
 def restricted_area():
-    key = bt.request.get_cookie("account", secret='some-secret-key')
-   # print(key)
-    #print(key[0],key[1])
-    #key=str(key)
-    #key=key.split(' ')
-    #print(key) #works!
-    #print(key2)
-    
-    #if check_login(key[0], key[1]):
-    if key:
+    global secret
+    if validate_user(secret=secret):
         return bt.static_file("upload_dataset.html",root="files/Template")
     else:
         return "You are not logged in. Access denied."
@@ -72,33 +114,61 @@ def restricted_area():
     
 @bt.get('/upload', method='POST')
 def do_upload():
-    upload     = bt.request.files.get('upload')
-    name, ext = os.path.splitext(upload.filename)
-    #if ext not in ('.png','.jpg','.jpeg'):
-        #return 'File extension not allowed.'
+    global clients
+    global secret
+    global clients_upload_threads
+    for c in clients:
+        key = bt.request.get_cookie(clients[c].username, secret=secret)
+        if key:
+            #valid user
+            newpath = r'files/Template/'+c+'/' 
+            if not os.path.exists(newpath):
+                os.makedirs(newpath)
+            t=threading.Thread(target=clients[c].uploadFile(newpath))
+            clients_upload_threads[c]=t
+            t.start()
+            return bt.redirect('/LoadingPredictor.html')
+        
+    return "You are not logged in. Access denied."
 
-    #save_path = get_save_path_for_category(category)
-    save_path='files/Template'
-    upload.save(save_path) # appends upload.filename automatically
-    return bt.redirect('/LoadingPredictor.html')
+
+@bt.get('/start')
+def GetPredictor():
+    global clients
+    global clients_run_threads
+    for c in clients:
+        key = bt.request.get_cookie(clients[c].username, secret=secret)
+        if key:
+            #Valid User
+            #client_username=c #=clients[c].username #
+            t=threading.Thread(target=clients[c].Run)
+            clients_run_threads[c]=t
+            t.start()
+            return "Started the Proccess!"
+            
+    return "You are not logged in. Access denied."
 
 @bt.get('/How_it_is_going')
-def GetPredictor():
-   global i
-   if(i>=100):
-       #redirect after
-       return str(i)+'%'
-   i+=1
-   return str(i)+'%'
+def Loading():
+    for c in clients:
+        key = bt.request.get_cookie(clients[c].username, secret=secret)
+        if key:
+            #Valid User
+            #client_username=c #=clients[c].username #
+#            if(clients[c].p.loading!="100%"):
+#                return clients[c].p.loading
+#            else:
+#                return bt.redirect("/Predictor.html")
+            return clients[c].p.loading
+    
+    return "You are not logged in. Access denied."
         
     
 
 @bt.get('/<filepath:path>')
 def server_static(filepath):
-    key = bt.request.get_cookie("account", secret='some-secret-key')
-    #key=key.split(' ')
     
-    if key:
+    if validate_user(secret='some-secret-key'):
         return bt.static_file(filepath, root='files/Template') 
     else:
         return "You are not logged in. Access denied."
