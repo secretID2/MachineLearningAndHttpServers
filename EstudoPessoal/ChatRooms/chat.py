6407d5d233bb4ca0bc709c3862b00276d05610c6
secretID2 <secretidentity009@gmail.com>
2018-01-23 17:49:39 +0000

    Access to rooms ok

--- EstudoPessoal/ChatRooms/chat.py
+++ EstudoPessoal/ChatRooms/chat.py
@@ -9,11 +9,19 @@ import bottle as bt
 from bottle import get, run
 from bottle.ext.websocket import GeventWebSocketServer
 from bottle.ext.websocket import websocket
+import threading
 
 users = []
 log=[]
 secret='nuncaVaodescobrr_ahah'
 chatRooms={}
+RoomUsers={}
+
+
+class ChatRoom():
+    
+    def __init__(self,thread,Name):
+        self.thread=thread
 
 @get('/')
 def index():
@@ -26,6 +34,7 @@ def LoadRooms():
         out='<form action="/accessRoom" method="post">'
         for room_name in chatRooms:
             out+='<br><p>'+room_name+'</p><br>'
+            out+='<input name="roomName" value="'+room_name+'" type="text" style="hidden"/>'
             out+='Password: <input name="password" type="password"  /><br>'
             out+='<input value="Enter" type="submit" /><br><br>'
         
@@ -41,6 +50,7 @@ def createRoom():
     global chatRooms
     roomName = bt.request.forms.get('roomName')
     password = bt.request.forms.get('password')
+    #print(roomName,password)
     chatRooms[roomName]=password
     bt.response.set_cookie(roomName, password, secret=secret)
     return bt.redirect('/'+roomName)
@@ -51,27 +61,27 @@ def accessRoom():
     global chatRooms
     roomName = bt.request.forms.get('roomName')
     password = bt.request.forms.get('password')
-    print(password,chatRooms[roomName])
+    #print(chatRooms[roomName])
     if password==chatRooms[roomName]:
         bt.response.set_cookie(roomName, password, secret=secret)
         return bt.redirect('/'+roomName)
     else:
         return 'Not Allowed to access this Room!'
     
-@bt.post('/<roomName>')
+@bt.get('/<roomName>')
 def enterRoom(roomName):
+    #print(roomName)
     global secret
     global chatRooms
     for room_name in chatRooms:
         key = bt.request.get_cookie(room_name, secret=secret)
-        print(key)
+        #print(key)
         if key==chatRooms[room_name]:
             return bt.static_file('chat.html',root='files/')
     return 'Not Allowed in this Room!'
 
-
 @get('/<roomName>/websocket', apply=[websocket])
-def chat(roomName,ws):
+def chat(ws):
     global log
     global secret
     global chatRooms
@@ -82,16 +92,13 @@ def chat(roomName,ws):
             if(ws!=None):
                 users.append(ws)
                 #Send conversation to new client
-                if(len(log)>0):
-                    for m in log:
-                        users[len(users)-1].send(m)
+                
                 ###################################
                 #Receive and send messages
                 while True:
                     msg = ws.receive()
                     print("received msg:",msg)
-                    if(msg is not None):
-                        log.append(msg)
+                    
                     if msg is not None:
                         for u in users:
                             try:
@@ -112,6 +119,52 @@ def chat(roomName,ws):
                 pass
         
     return 'Not allowed to Send message!!'
+
+#@get('/<roomName>/websocket', apply=[websocket])
+#def handleChat(roomName,ws):
+#    t=threading.Thread(target=chat(roomName,ws))
+#    UsersChatThread[roomName]=t
+#    t.start()
+#
+#
+#def chat(roomName,ws):
+#    global log
+#    global secret
+#    global chatRooms
+#    for room_name in chatRooms:
+#        key = bt.request.get_cookie(room_name, secret=secret)
+#        if key==chatRooms[room_name]:
+#        
+#            if(ws!=None):
+#                users.append(ws)
+#                #Send conversation to new client
+#                
+#                ###################################
+#                #Receive and send messages
+#                while True:
+#                    msg = ws.receive()
+#                    print("received msg:",msg)
+#                    
+#                    if msg is not None:
+#                        for u in users:
+#                            try:
+#                                u.send(msg)
+#                            except:
+#                                print("error sending message")
+#                                pass
+#                    else:
+#                        break
+#                ##################################
+#                try:    
+#                    users.remove(ws)
+#                except:
+#                    print("error removing ws")
+#                    pass
+#            else:
+#                print("Someone call /websocket without websocket")
+#                pass
+#        
+#    return 'Not allowed to Send message!!'
 
 
 
